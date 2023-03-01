73dd9e60d591fd8ae7c50e808b0e7b5a9fc5bdd1
Denis Xarkov <97855565+DenisXarkov@users.noreply.github.com>
2023-03-02 00:07:09 +0300

    Домашние задания с learn.javascript

---
+++ learnjs-homework/hw-2/script.js
@@ -0,0 +1,11 @@
+let admin;
+let name;
+name= 'Джон';
+admin=name;
+alert(admin);
+
+
+let planetName= 'Земля';
+let userName = 'Семен';
+
+
---
+++ learnjs-homework/hw-4/script.js
@@ -0,0 +1,2 @@
+let userName = prompt("Какое ваше имя","")
+alert(userName)
